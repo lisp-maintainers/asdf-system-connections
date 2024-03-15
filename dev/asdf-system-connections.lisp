cd29e4615693541ed0861df97f7ec3fdf1d64c6f
digikar <shubhamkarayare@gmail.com>
2024-03-16 01:29:06 +0530

    Load systems selectively by maintaining a *system-connections* table

--- dev/asdf-system-connections.lisp
+++ dev/asdf-system-connections.lisp
@@ -12,50 +12,68 @@
 
 ;;; ---------------------------------------------------------------------------
 
-(defun map-system-connections (fn)
-  (map-systems
-   (lambda (s) (when (typep s 'system-connection) (funcall fn s)))))
-
-;;; ---------------------------------------------------------------------------
+(defvar *system-connections* (make-hash-table :test 'equal))
 
 (defmacro defsystem-connection (name &body options)
   (let ((requires (getf options :requires))
-        (class (getf options :class 'system-connection)))
+        (depends-on (getf options :depends-on))
+        (class (getf options :class 'system-connection))
+        (connections (gensym "CONNECTIONS")))
     (remf options :requires)
     (remf options :class)
     `(progn
        (defsystem ,name
          :class ,class
-         :depends-on ,requires
+         :depends-on ,(append requires
+                              depends-on)
          :systems-required ,requires
          ,@options)
+       ,@(mapcar (lambda (r)
+                   (flet ((system-name (s)
+                            (etypecase s
+                              (string s)
+                              (symbol (string-downcase (symbol-name s)))
+                              (asdf:component (asdf:component-name s)))))
+                     (let* ((r    (system-name r))
+                            (name (system-name name))
+                            (requires
+                              (sort (mapcar #'system-name requires) #'string<)))
+                       `(let ((,connections
+                                (append (gethash ',r *system-connections*)
+                                        (list (cons (remove ',r ',requires
+                                                            :test #'string=)
+                                                    ',name)))))
+                          (setf (gethash ',r *system-connections*)
+                                ,connections)))))
+                 requires)
        (values ',name))))
 
 ;;; ---------------------------------------------------------------------------
 
-(defun load-connected-systems ()
-  (map-system-connections
-   (lambda (connection)
-     (when (and (required-systems-loaded-p connection)
-                (not (system-loaded-p (component-name connection))))
-       (load-system (component-name connection))))))
-
-(defun required-systems-loaded-p (connection)
-  (every #'system-loaded-p (systems-required connection)))
-
-;;; ---------------------------------------------------------------------------
-(unless (fboundp 'registered-system)
-  (defun registered-system (system-name)
-    (cdr (system-registered-p system-name))))
-
-(defun system-loaded-p (system-name)
-  (if-let (it (registered-system system-name))
-    (component-operation-time (make-operation 'load-op) it)))
+(defun load-connected-systems (operation component)
+  (let ((connections (gethash (typecase component
+                                (string component)
+                                (symbol (string-downcase (symbol-name component)))
+                                (asdf:component (asdf:component-name component)))
+                              *system-connections*)))
+    (loop :for (prerequisites . connection) :in connections
+          :do (when (and (not (component-loaded-p connection))
+                         (every #'component-loaded-p prerequisites))
+                (asdf:oos operation connection)))))
 
 ;;; ---------------------------------------------------------------------------
 
 (defmethod operate :after ((operation t) (component t) &key &allow-other-keys)
-  (load-connected-systems))
+  (when (or (eq 'asdf:load-op operation)
+            (typep operation 'asdf:load-op))
+    (loop :for dep :in (system-depends-on
+                        (etypecase component
+                          (system
+                           component)
+                          ((or string symbol)
+                           (find-system component))))
+          :do (load-connected-systems 'asdf:load-op dep))
+    (load-connected-systems 'asdf:load-op component)))
 
 ;;; ---------------------------------------------------------------------------
 

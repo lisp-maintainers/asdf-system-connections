b9efa55674bd200ea2efa8059316514df112b920
digikar <shubhamkarayare@gmail.com>
2024-03-16 10:09:26 +0530

    Do not save connection to a dependency

--- dev/asdf-system-connections.lisp
+++ dev/asdf-system-connections.lisp
@@ -12,13 +12,49 @@
 
 ;;; ---------------------------------------------------------------------------
 
+(defun find-system-from-dep-spec (depends-on)
+  (etypecase depends-on
+    (system
+     depends-on)
+    ((or string symbol)
+     (find-system depends-on nil))
+    (list
+     (find-system
+      (let ((spec (ecase (first depends-on)
+                    (:version (second depends-on))
+                    (:feature (third depends-on)))))
+        (etypecase spec
+          ((or string symbol) spec)
+          (list
+           (ecase (first spec)
+             (:require (second spec))))))
+      nil))))
+
+(defun system-depends-on-p (system depends-on)
+  "Returns non-NIL if DEPENDS-ON is a dependency of SYSTEM."
+  (let* ((system (find-system-from-dep-spec system))
+         (depends-on (find-system-from-dep-spec depends-on))
+         (depends-on-name (when depends-on
+                            (component-name depends-on))))
+    (when (and system depends-on)
+      (some (lambda (dep)
+              (let* ((dep (find-system-from-dep-spec dep))
+                     (dep-name (component-name dep)))
+                (or (string= dep-name depends-on-name)
+                    (loop :for dep-dep :in (system-depends-on dep)
+                            :thereis (system-depends-on-p dep-dep depends-on)))))
+            (system-depends-on system)))))
+
+;;; ---------------------------------------------------------------------------
+
 (defvar *system-connections* (make-hash-table :test 'equal))
 
 (defmacro defsystem-connection (name &body options)
   (let ((requires (getf options :requires))
         (depends-on (getf options :depends-on))
         (class (getf options :class 'system-connection))
-        (connections (gensym "CONNECTIONS")))
+        (connections (gensym "CONNECTIONS"))
+        (prerequisites (gensym "PREREQUISITES")))
     (remf options :requires)
     (remf options :class)
     `(progn
@@ -35,32 +71,30 @@
                               (symbol (string-downcase (symbol-name s)))
                               (asdf:component (asdf:component-name s)))))
                      (let* ((r    (system-name r))
+                            (req  (gensym "REQ"))
                             (name (system-name name))
                             (requires
                               (sort (mapcar #'system-name requires) #'string<)))
-                       `(let ((,connections
-                                (append (gethash ',r *system-connections*)
-                                        (list (cons (remove ',r ',requires
-                                                            :test #'string=)
-                                                    ',name)))))
-                          (setf (gethash ',r *system-connections*)
-                                ,connections)))))
+                       `(let* ((,prerequisites
+                                 (remove-if
+                                  (lambda (,req)
+                                    (system-depends-on-p ,req ',r))
+                                  (remove ',r ',requires
+                                          :test #'string=)))
+                               (,connections
+                                 (append (gethash ',r *system-connections*)
+                                         (list (cons ,prerequisites
+                                                ',name)))))
+                          (when ,prerequisites
+                            (setf (gethash ',r *system-connections*)
+                                  ,connections))))))
                  requires)
        (values ',name))))
 
 ;;; ---------------------------------------------------------------------------
 
 (defun load-connected-systems (operation component)
-  (let* ((component (etypecase component
-                      (system
-                       component)
-                      ((or string symbol)
-                       (find-system component))
-                      (list
-                       (find-system
-                        (ecase (car component)
-                          (:version (second component))
-                          (:feature (third component)))))))
+  (let* ((component (find-system-from-dep-spec component))
          (deps (system-depends-on component))
          (connections (gethash (component-name component)
                                *system-connections*)))
